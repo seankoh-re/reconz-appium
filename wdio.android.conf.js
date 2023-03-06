30e1319b0c7bff1583696db06ed2711e0401ecea
Seankoh <sean.koh@realestate.co.nz>
2023-03-06 16:15:26 +1300

    Update yml, package and config files

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -4,10 +4,9 @@ config.capabilities = [
   {
     platformName: 'Android',
     'appium:automationName': 'UIAutomator2',
-    'appium:app':
-      '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
+    'appium:app': './apk/app-dev-debug.apk',
+    // '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
   },
 ];
 
-//'./apk/app-dev-debug.apk',
 exports.config = config;
