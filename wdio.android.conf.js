c89ac515cb7b52fb6a63ff86ffaf3b81479c4a08
Seankoh <sean.koh@realestate.co.nz>
2023-03-06 16:29:08 +1300

    Adding back platform and automation name

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -2,8 +2,8 @@ const { config } = require('./wdio.conf');
 
 config.capabilities = [
   {
-    //platformName: 'Android',
-    //'appium:automationName': 'UIAutomator2',
+    platformName: 'Android',
+    'appium:automationName': 'UIAutomator2',
     'appium:app': './apk/app-dev-debug.apk',
     // '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
   },
