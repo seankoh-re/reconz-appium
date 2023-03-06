5ad0058dd25987b6c83293eaca852cc8d786d35d
Seankoh <sean.koh@realestate.co.nz>
2023-03-06 13:59:03 +1300

    Add APK, change path to apk

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -6,7 +6,7 @@ config.capabilities = [
     'appium:deviceName': 'Pixel XL',
     'appium:platformVersion': '13.0',
     'appium:automationName': 'UIAutomator2',
-    //'appium:app':
+    'appium:app': './apk/app-dev-debug.apk',
     //  '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
   },
 ];
