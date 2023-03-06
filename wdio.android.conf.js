de1dd569c1d731268bf792f8810c0cc20e48c052
Seankoh <sean.koh@realestate.co.nz>
2023-03-06 15:41:24 +1300

    update capabilities and api level

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -3,12 +3,11 @@ const { config } = require('./wdio.conf');
 config.capabilities = [
   {
     platformName: 'Android',
-    'appium:deviceName': 'Pixel XL',
-    'appium:platformVersion': '13.0',
     'appium:automationName': 'UIAutomator2',
-    'appium:app': './apk/app-dev-debug.apk',
-    //  '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
+    'appium:app':
+      '/Users/seankoh/RE/reconz-app/android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
   },
 ];
 
+//'./apk/app-dev-debug.apk',
 exports.config = config;
